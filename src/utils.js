127a91ab209e502a7924e2b265095f05a0c9c5ec
RaMs94 <41548350+RaMs94@users.noreply.github.com>
2019-05-21 01:58:09 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -11,3 +11,14 @@ export async function leggeMeteo(entita, lat, lon, api_key) {
   return ris;
 }
 
+export async function leggeMeteoSettimanale(api_key, lat, lon) {
+  let str = `https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${api_key}&q=${lat},${lon}&format=json&num_of_days=7&tp=24`;
+  let ris;
+  await fetch(str)
+    .then(response => response.json())
+    .then(data => {
+      ris = data;
+    })
+    .catch(e => console.log("error", e));
+  return ris;
+}
